eb78b6d5bafc5b6a1c08e746e78e1af81a715949
Franco Caramia <franco.caramia@petermac.org>
2013-10-03 13:58:04 +1000

    sliding window function

--- window.py
+++ window.py
@@ -1,4 +1,4 @@
-def slide_window()
+def slide_window(scores,window_size,threshold)
     '''
     This function procceses the quality scores from the sequence
     Input: 
@@ -9,8 +9,39 @@ def slide_window()
         5'end cut index
         3'end cut index
     '''
+    size = len (scores)
+    #Averages of all sliding windows
+    averages = []
+    for x in range(size-window_size)
+        averages.append(calc_avg(scores[x:x+window_size]))
     
-    
+    first = ''
+    second = ''
+    x = 0
+    #If average started bad , slide until we get a good one
+    if averages[0] < threshold:
+        while averages[x] < threshold and x < len(averages):
+            first  = x
+            x+=1
+        #Found good average or end of seq, keep sliding until gets bad again
+        while averages[x] > threshold and x < len(averages):
+            second = x
+            x=+1
+        #If the reads were never good return 0 0
+        if second == '':
+            return 0,0
+        else:
+            return first, second
+                
+    else:
+        #If we started with a good average slide until we find a bad one
+        first = x
+        while averages[x] > threshold and x < len(averages):
+            second = x
+            x=+1
+        
+        return first, second
+        
     
 def calc_avg(scores)
     '''
