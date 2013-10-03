8963c080b4958ad2fbb8678fa9b706600f99be0a
Maria Doyle <maria.doyle@petermac.org>
2013-10-03 14:56:28 +1000

    Edited some comments in Sliding window module

--- window.py
+++ window.py
@@ -2,48 +2,46 @@ import doctest
 
 def slide_window(scores,window_size,threshold):
     '''
-    This function procceses the quality scores from the sequence
+    This function analyses quality scores from a read using a sliding window and determines what region is good quality (region where average quality score is above specified threshold)
     Input: 
         scores:list with quality scores 
         window_size: size of window to calculate avg
-        threshold: threshold to trimm sequence
+        threshold: threshold to trim sequence
     Output:
         5'end cut index
         3'end cut index
    
-    #Case 1: read starts good ends bad
+    #Test case 1: read starts good ends bad
     >>> slide_window([30,30,30,30,30,5,5,5,5],4,15)
     (0, 6)
     
-    #Case2: read starts bad ends good
+    #Test case2: read starts bad ends good
     >>> slide_window([5,5,5,5,5,30,30,30,30,30],4,15)
     (3, 9)
      
      '''
     
     
-    
-  
+    #Calculate average quality scores for windows
     size = len(scores)
-    #Averages of all sliding windows
     averages = []
     for x in range(size-window_size+1):
         averages.append(calc_avg(scores[x:x+window_size]))
     
-    first = ''
-    second = ''
+    first = '' #5' index to cut
+    second = ''#3 index to cut
     x = 0
     #If average started bad, slide until we get a good one
     if averages[0] < threshold:
         while x < len(averages) and averages[x] < threshold:
             x+=1
             first  = x           
-        #Found good average or end of seq, keep sliding until gets bad again
+        #If found good average or end of seq, keep sliding until gets bad again
         while x < len(averages) and averages[x] > threshold:
             second = x
             x+=1
             
-        #If the read scores are all below threshold return 0 0
+        #If the read scores are all below threshold return -1,-1 which tells the trimming module to reject the read
         if second == '':
             return -1,-1
         else:
